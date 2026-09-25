72dadcd89f87c2d052a66e653ccfa82c36c040b7
kmoshkov <kmoshkov7@gmail.com>
2026-09-25 13:06:26 +0400

    Lab 1 modified – tasks 2.3 and 2.2.

--- lab1/sol2.sh
+++ lab1/sol2.sh
@@ -5,17 +5,10 @@ mkdir -p data_analysis
 (cut -d ',' -f 3 faculty_4.csv | tr "+" "\n" | sed 's/^ *//; s/ *$//' | sort | uniq) > ./data_analysis/research_themes.txt
 (cat ./data_analysis/research_themes.txt | wc -l ) >> ./data_analysis/research_themes.txt
 
-(cut -d ',' -f 3 faculty_4.csv | tr "+" "\n" | sed 's/^ *//; s/ *$//' | sort | uniq -c | awk '$1 > 12 { $1 = ""; print $0 }') > ./data_analysis/very_big_groups.txt
 
-awk -F ',' '
-    NR==FNR {
-        if ($3 ~ /Artificial Intelligence/) { ai[$1] = 1 }
-        next
-    }
- 
-    $3 ~ /Machine Learning/ {
-        if ($1 in ai) { print $0 }
-    }
-' faculty_4.csv faculty_4.csv > ./data_analysis/joined.csv
+( cut -d "," -f 3 faculty_4.csv| tr "+" "\n" | sed 's/^ *//; s/ *$//' | sort | uniq -c | sort -n | awk '$1 > 12 {$1 = ""; print $0}' | sed 's/^ *//; s/ *$//') > ./data_analysis/very_big_groups.txt
 
-(cut -d ',' -f 1 faculty_4.csv | sed 's/^ *//; s/ *$//' | sort -k 2 ) > ./data_analysis/sorted_names.txt
+
+(cat faculty_4.csv | grep 'Artificial Intelligence' | grep 'Machine Learning') > ./data_analysis/joined.csv 
+
+(cut -d "," -f 1 faculty_4.csv| sort -k 2) > ./data_analysis/sorted_names.txt
